3a8fe7dec863b29b481d8fd4808ed58fadbb3fcd
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2022-12-27 17:16:35 +0300

    feat(store): add news selector

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import accountReduser from "./slices/account/accountSlice";
 import themeReduser from "./slices/theme/themeSlice";
 import articlesReduser from "./slices/articles/articlesSlice";
+import newsReduser from "./slices/news/newsSlice";
 
 export const store = configureStore({
   reducer: {
     account: accountReduser,
     theme: themeReduser,
     articles: articlesReduser,
+    news: newsReduser,
   },
 });
 
