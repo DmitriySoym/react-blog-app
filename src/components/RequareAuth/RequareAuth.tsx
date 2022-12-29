d1aca0895f31dfcba26bd5abdedf648371121e15
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2022-12-29 12:30:21 +0300

    feat(components): add requare auth component

---
+++ src/components/RequareAuth/RequareAuth.tsx
@@ -0,0 +1,7 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { ROUTE } from "../../router";
+
+export const RequareAuth = () => {
+  const isAuth = true;
+  return isAuth ? <Outlet /> : <Navigate to={ROUTE.HOME} />;
+};
