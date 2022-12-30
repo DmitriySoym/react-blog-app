c51ede91489fb9f46b25bebae4c75980dc4e0525
Dmitriy Soymchenko <d.soymchenko@gmail.com>
2022-12-30 12:57:37 +0300

    feat(pages): implement loading content on details page

--- src/pages/DetailsPage.tsx
+++ src/pages/DetailsPage.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { IArticle } from "types";
 
 export const DetailsPage = () => {
-  const params = useParams();
+  const [article, setArticle] = useState({} as IArticle);
+  const { id } = useParams();
 
-  return <>{params.id}</>;
+  useEffect(() => {
+    fetch(`https://api.spaceflightnewsapi.net/v3/articles/${id}`)
+      .then((response) => response.json())
+      .then(setArticle);
+  }, [id]);
+
+  return (
+    <>
+      <span>{id}</span>
+      {article.title} <img src={article.imageUrl} />
+    </>
+  );
 };
